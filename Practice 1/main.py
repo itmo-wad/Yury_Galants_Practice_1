2e58d71335588e50fa4955529f93e21f02832b40
T4keTheDive <101790490+T4keTheDive@users.noreply.github.com>
2022-03-25 21:22:23 +0300

    completed

--- Practice 1/main.py
+++ Practice 1/main.py
@@ -1,15 +1,55 @@
-from flask import Flask
-from flask_httpauth import HTTPBasicAuth
+from flask import Flask, render_template, request, redirect, send_from_directory
+from pymongo import MongoClient
 
 app = Flask(__name__)
-auth = HTTPBasicAuth()
+mongo = MongoClient(‘localhost’,27017)
+db = mongo.pratice
 
+app.config["UPLOAD_FOLDER"]= ‘imgupl'
 
-@app.route('/')
-@auth.login_required
+@app.route(‘/’)
 def index():
-    return "Hello, %s!" % auth.username()
+    return render_template(‘index.html’,users=user)
+
+@app.route(‘/signup’)
+def signup():
+    return render_template(‘signup.html’)
+
+@app.route('/auth', methods=['GET', 'POST'])
+def auth():
+    if request.method == 'POST':
+        username = request.form.get('username', 0)
+        password = request.form.get('password', 0)
+        if not (username and password):
+            return 'Error'
+
+        data = list(db.users.find({'username' : username}, {'password' : password}))
+        if len(data):
+            data = data[0]['password']
+            print(data)
+            if password == data:
+                return render_template('login.html')
+        return 'Error'
+
+    return render_template('login.html')
+
+ALLOWED_EXTENSIONS = {‘png’, 'gif', ‘jpg’, ‘jpeg’}
+@app.route(‘/upload’,methods=[‘GET’,’POST’])
+def imgupl(): 
+    if request.method == ‘POST’:
+        this_file = request.files[‘file’]
+        
+        if this_file in ALLOWED_EXTENSIONS:
+            this_file.save(os.path.join(app.config[‘UPLOAD_FOLDER’],filename))
+            return ‘Successfully uploaded’
+        else:
+            return (‘Error’)
+    return render_template(‘imgupl.html’)
+
+@app.route(‘/upload/<filename>‘)
+def imguplfile(filename):
+    return send_from_directory(app.config[‘UPLOAD_FOLDER’],filename)
 
 
-if __name__ == '__main__':
-    app.run(host='localhost', port=5001, debug=True)
+if __name__ == ‘__main__’:
+    app.run(host=‘localhost’, port=5000, debug=True)
